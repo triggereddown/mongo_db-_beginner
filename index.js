7d01dd3ee60c82627dc8011dbe2ca4cb72426dec
triggereddown <deepmoitra2@gmail.com>
2024-04-11 18:13:15 +0530

    Accessing SSR to the CSR

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const button1 = document.getElementById("button1");
+//     const personDataDiv = document.getElementById("personData");
 
-const goToPersonRoute = () => {
-    window.location.href = "/person"; // Navigate to the '/person' route
-}
+//     button1.addEventListener("click", () => {
+//         fetch('/person')
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             // Display person data in the div
+//             data.forEach(person => {
+//                 personDataDiv.innerHTML += `
+//                     <h2>Person Data</h2>
+//                     <p>Name: ${person.name}</p>
+//                     <p>Age: ${person.age}</p>
+//                     <p>Work: ${person.work}</p>
+//                 `;
+//             });
+//         })
+//         .catch(error => console.error('Error fetching person data:', error));
+//     });
+// });
 
-const goToMenuRoute = () => {
-    window.location.href = "/menu"; // Navigate to the '/menu' route
-}
 
-button1.addEventListener("click", goToPersonRoute);
-button2.addEventListener("click", goToMenuRoute);
+// document.addEventListener("DOMContentLoaded", function() {
+//     const button1 = document.getElementById("button2");
+//     const personDataDiv = document.getElementById("menuData");
 
+//     button1.addEventListener("click", () => {
+//         fetch('/menu')
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             // Display person data in the div
+//             data.forEach(person => {
+//                 personDataDiv.innerHTML += `
+//                     <h2>Person Data</h2>
+//                     <p>Name: ${person.name}</p>
+//                     <p>Price: ${person.price}</p>
+//                     <p>Taste: ${person.taste}</p>
+//                 `;
+//             });
+//         })
+//         .catch(error => console.error('Error fetching person data:', error));
+//     });
+// });
